3ce9312b1770905a0e00996dea8d5038fcdc000c
jeeva <jeevaprakashdr@gmail.com>
2024-01-15 14:53:52 +0100

    structure cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[derive(Debug)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("no command is provided");
+    let path = std::env::args().nth(2).expect("no path is given");
+
+    let args = Cli{
+        pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
+    println!("{:?}", args);
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
 }
