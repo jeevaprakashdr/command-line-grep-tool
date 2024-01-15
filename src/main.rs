d607aed891b8d32330f44ecf65a454afba03314d
jeeva <jeevaprakashdr@gmail.com>
2024-01-15 22:13:26 +0100

    error reporting through anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use std::{io::{BufReader, BufRead}, fs::File};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
+use anyhow::Context;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -8,16 +12,19 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args = Cli::parse();
-    let inner = File::open(&args.path).expect("cannot open file");
-    let reader = BufReader::new(inner);
-    
-    for line in reader.lines(){
+    let file = File::open(&args.path).with_context(|| format!("could not find/open file: {}", args.path.display()))?;
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
         match line {
-            Ok(str) if str.contains(&args.pattern)  => println!("{}", str),
+            Ok(str) if str.contains(&args.pattern) => println!("{}", str),
             Ok(_) => (),
             Err(_) => (),
         }
     }
+
+    Ok(())
 }
