5a7fb47e98ec53640f5d0b3fced14331b26c8f60
jeeva <jeevaprakashdr@gmail.com>
2024-01-16 09:03:02 +0100

    add unit test for find match function.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{string, fmt::format};
+use std::{string, fmt::format, result};
 
 use anyhow::Context;
 use clap::Parser;
@@ -17,11 +17,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let content = std::fs::read_to_string(&args.path)
     .with_context(|| format!("Count not read file '{}'", args.path.display()))?;
 
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    Ok(())
+}
+
+fn find_matches(content:&str, pattern: &str, mut writer: impl std::io::Write){
     for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line)
+        if line.contains(&pattern) {
+            writeln!(writer, "{}", line);
         }
     }
-    
-    Ok(())
+}
+
+#[test]
+fn test_find_a_match() {
+    let mut result = Vec::new();
+    find_matches("little known place has a lot to give", "has", &mut result);
+    assert_eq!(result, b"little known place has a lot to give\n");
 }
